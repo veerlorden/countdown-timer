b5d439c47bb5d4fbf3c6a2c460b0e9c9776bff29
veerlorden <7z95A0dJ73h213>
2021-02-25 19:31:36 +0300

    added favicons and changed the year to 2022

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const startCountdown = (targetDate) => {
   const $days = document.getElementById('days')
   const $hours = document.getElementById('hours')
-  const $mins = document.getElementById('mins')
+  const $minutes = document.getElementById('minutes')
   const $seconds = document.getElementById('seconds')
 
   function formatDate(time) {
@@ -12,20 +12,29 @@ const startCountdown = (targetDate) => {
     const currentDate = new Date()
     const diff = new Date(targetDate) - currentDate
 
+    if (diff < 0) {
+      clearInterval(intervalID)
+      const tag = document.createElement('h1')
+      const app = document.getElementById('app')
+      tag.textContent = 'Новый год уже наступил! Пожалуйста, введите новую дату'
+      app.textContent = ''
+      app.insertAdjacentElement('afterbegin', tag)
+    }
+
     const secondsRemain = diff / 1000
 
     const days = Math.floor(secondsRemain / 60 / 60 / 24)
     const hours = Math.floor(secondsRemain / 60 / 60 % 24)
-    const mins = Math.floor(secondsRemain / 60 % 60)
+    const minutes = Math.floor(secondsRemain / 60 % 60)
     const seconds = Math.floor(secondsRemain % 60)
 
-    $days.textContent = days
+    $days.textContent = days.toString()
     $hours.textContent = formatDate(hours)
-    $mins.textContent = formatDate(mins)
+    $minutes.textContent = formatDate(minutes)
     $seconds.textContent = formatDate(seconds)
   }
 }
 
-const countdown = startCountdown('1 Jan 2021')
+const countdown = startCountdown('1 Jan 2022')
 
-setInterval(countdown, 1000)
+const intervalID = setInterval(countdown, 1000)
